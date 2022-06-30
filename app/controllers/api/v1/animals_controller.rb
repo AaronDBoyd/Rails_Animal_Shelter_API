dd543a59423917e379af4aef7f7283754ddf9516
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-30 14:16:47 -0700

    add animal_params method

--- app/controllers/api/v1/animals_controller.rb
+++ app/controllers/api/v1/animals_controller.rb
@@ -1,4 +1,28 @@
 class Api::V1::AnimalsController < ApplicationController
 
+  def index
 
+  end
+
+  def show
+
+  end
+
+  def create!
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def animal_params
+    params.permit(:name, :age, :breed)
+  end
 end
