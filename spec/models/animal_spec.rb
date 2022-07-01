3c49076a76c317e29a94ffb386c9f203d0106d2e
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-30 17:08:15 -0700

    pass tests for validating presence of name

--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Animal, type: :model do
-  
   it { should validate_presence_of :name }
+  it { should validate_presence_of :age }
+  it { should validate_presence_of :breed }
 end
