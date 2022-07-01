eb422f64ed50fad299b9b83201b3c07b742ce518
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-30 18:27:51 -0700

    titleize_animal method

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,4 +2,14 @@ class Animal < ApplicationRecord
   validates :name, presence: true
   validates :age, presence: true
   validates :breed, presence: true
+
+
+
+  before_save(:titleize_animal)
+
+  private
+
+  def titleize_animal
+    self.name = self.name.titleize
+  end
 end
