3fe47df53a9f0a4fda7fb6944cde3ce4195f79e4
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-30 19:13:25 -0700

    pass spec for titleizing animal name

--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Animal, type: :model do
   it { should validate_presence_of :name }
   it { should validate_presence_of :age }
   it { should validate_presence_of :breed }
+
+  it 'titleizes the name of an animal' do
+    animal = Animal.create(name: 'steve',age: 13, breed: 'pupper')
+    expect(animal.name).to eq('Steve')
+  end
 end
