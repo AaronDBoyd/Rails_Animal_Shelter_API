cde4fa2e0262de1052a680dc781c35bc2bd25cc7
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-30 18:30:34 -0700

    add search_name and search_breed methods

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,7 +3,21 @@ class Animal < ApplicationRecord
   validates :age, presence: true
   validates :breed, presence: true
 
+  def self.search_name(search)
+    if search
+      where(["name ILIKE ?","%#{search}%"])
+    else
+      all
+    end
+  end 
 
+  def self.search_breed(search)
+    if search
+      where(["breed ILIKE ?","%#{search}%"])
+    else
+      all
+    end
+  end 
 
   before_save(:titleize_animal)
 
