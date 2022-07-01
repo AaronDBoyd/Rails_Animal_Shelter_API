725b43f6ebc3491ce88daa48223b320ad9224883
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-30 19:27:27 -0700

    pass first spec

--- spec/requests/get_routes_spec.rb
+++ spec/requests/get_routes_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
-describe 'get animals route' :type => :request do
+describe "get animals route", :type => :request do
   let!(:animals) { FactoryBot.create_list(:animal, 20)}
 
+  before { get '/api/v1/animals' }
+
+  it 'returns all animals' do
+    expect(JSON.parse(response.body).size).to eq(20)
+  end
 end
